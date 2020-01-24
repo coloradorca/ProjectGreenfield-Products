6674ec4567e78692383ed4455f363b065d8ce4a9
Savannah Williams <Savi@YeeHaw.local>
2020-01-24 11:00:42 -0700

    Install react router and sass. Adds access to route in App.jsx. Creates folder for style and a blank App.scss folder for style and is imported in App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ProductOverview from './ProductOverview/ProductOverview.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
+
+import '../Styles/App.scss';
 
 class App extends React.Component {
   constructor(props) {
