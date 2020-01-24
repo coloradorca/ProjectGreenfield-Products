773a36035926f65c565a3b0e30d682c0361aee1b
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-01-23 17:11:07 -0700

    Webpack configured

---
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class QuestionsAndAnswers extends React.Component {
+
+  render() {
+    return (
+      <div>Questions And Answers</div>
+    )
+  }
+}
+
+export default QuestionsAndAnswers;
