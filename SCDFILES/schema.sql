b0d478f36d088416c37bc3014979788c8b110665
Rob Gonzalez-Pita <robgonzalezpita@gmail.com>
2020-02-14 14:46:27 -0700

    added sql script files

---
+++ SCDFILES/schema.sql
@@ -0,0 +1,73 @@
+
+DROP DATABASE IF EXISTS examples;
+
+create database examples;
+
+\c examples;
+
+CREATE TABLE product (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  slogan TEXT,
+  description TEXT,
+  category TEXT NOT NULL,
+  default_price INTEGER
+);
+
+CREATE TABLE styles (
+  -- id SERIAL PRIMARY KEY,
+  id INT,
+  productId INTEGER,
+  name TEXT,
+  sale_price TEXT,
+  original_price INTEGER,
+  default_style INTEGER,
+  FOREIGN KEY (productId) REFERENCES product (id)
+);
+
+CREATE TABLE features (
+  -- id SERIAL PRIMARY KEY,
+  id INT,
+  style_id INTEGER,
+  val TEXT,
+  attribute TEXT,
+  FOREIGN KEY (style_id) REFERENCES styles (id)
+);
+
+CREATE TABLE skus (
+  -- id SERIAL PRIMARY KEY,
+  id INT,
+  style_id INTEGER,
+  size TEXT,
+  quantity INTEGER,
+  FOREIGN KEY (style_id) REFERENCES styles (id)
+);
+
+CREATE TABLE photos (
+  -- id SERIAL PRIMARY KEY,
+  id INT,
+  style_id INTEGER,
+  url TEXT,
+  thumbnail_url TEXT,
+  FOREIGN KEY (style_id) REFERENCES styles (id)
+);
+
+COPY product
+FROM
+  '/Users/robgonzalez-pita/Desktop/UglyDog/PO/ProjectGreenfield-Products/SDCFiles/Sample10/SampleProduct.csv' DELIMITER ',' CSV HEADER;
+
+COPY styles
+FROM
+  '/Users/robgonzalez-pita/Desktop/UglyDog/PO/ProjectGreenfield-Products/SDCFiles/Sample10/stylesSample.csv' DELIMITER ',' CSV HEADER;
+
+COPY features
+FROM
+  '/Users/robgonzalez-pita/Desktop/UglyDog/PO/ProjectGreenfield-Products/SDCFiles/Sample10/SampleFeatures.csv' DELIMITER ',' NULL AS 'null' CSV HEADER;
+
+COPY skus
+FROM
+  '/Users/robgonzalez-pita/Desktop/UglyDog/PO/ProjectGreenfield-Products/SDCFiles/Sample10/SampleSKUS.csv' DELIMITER ',' CSV HEADER;
+
+COPY photos
+FROM
+  '/Users/robgonzalez-pita/Desktop/UglyDog/PO/ProjectGreenfield-Products/SDCFiles/Sample10/SamplePhotos.csv' DELIMITER ',' CSV HEADER;
